78c95e7b0eaf652c76cbcdafe561020cfd3a0456
woodrowdeveloper <woodrowdeveloper@gmail.com>
2021-09-18 16:11:03 -0400

    Add project files.

---
+++ Features/Login.feature
@@ -0,0 +1,11 @@
+﻿Feature: Login
+	Check if login functionality works
+
+@mytag
+Scenario: Login user as Administrator 
+	Given I navigate to application
+	And I enter username and password
+	| UserName | Password |
+	| admin    | admin    |
+	And I click login 
+	Then I should see user logged in 
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SpecFlowParallelTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SpecFlowParallelTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b2be6e3-dc9f-4e03-856a-5620f55ee807")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
